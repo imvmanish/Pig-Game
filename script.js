1c2ee8d49903046ab21bd4ba603e2ca444bbc8fd
Manish V <imvmanish18@gmail.com>
2021-11-03 14:06:57 +0530

    added hold score functionality

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 'use strict';
 const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
 const dice = document.querySelector('.dice');
-const currentAScoreElement = document.querySelector('#current--0'); 
-const currentBScoreElement = document.querySelector('#current--1');
-const totalAScoreElement = document.querySelector('#score--0'); 
-const totalBScoreElement = document.querySelector('#score--1');
-let totalAScore,totalBScore,currentAScore,currentBScore;
+const currentElement = [document.querySelector('#current--0'),document.querySelector('#current--1')];
+const totalElement = [document.querySelector('#score--0'),document.querySelector('#score--1')];
+const playerActive = document.querySelector('.player--active');
+const playerWinner = document.querySelector('.player--winner');
+
+//Player Main Elements
+let activePlayer;
+let totalScores,currentScores;
 
 function init() {
-    totalAScore = 0;
-    totalBScore = 0;
-    currentAScore = 0;
-    currentBScore = 0;
-    totalAScoreElement.textContent = 0;
-    totalBScoreElement.textContent = 0;
+    currentScores = [0,0];
+    totalScores = [0,0];
+    totalElement[0].textContent = 0;
+    totalElement[1].textContent = 0;
+    activePlayer = 0;
 }
 init();
 
+function switchPlayer() {
+    activePlayer = !activePlayer;
+}
+
 buttonRoll.addEventListener('click',function() {
     let random = Math.floor(Math.random() * 6) + 1;
-    init();
     dice.src = `dice-${random}.png`;
+    if(random>1) {
+        currentScores[activePlayer] += random;
+        currentElement[activePlayer].textContent = currentScores[activePlayer];
+    }
+    else {
+        currentScores[activePlayer] = 0;
+    }
+});
+
+buttonHold.addEventListener('click',function() {
+    totalScores[activePlayer] += currentScores[activePlayer];
+    totalElement[activePlayer].textContent = totalScores[activePlayer];
+    switchPlayer();
 });
 
